825eef3f1d526ab9e4c345a367eba404f9008779
shivamvr <shivamweb2@gmail.com>
2022-12-26 16:33:40 +0530

    day-1

---
+++ day_1/problem4.js
@@ -0,0 +1,15 @@
+var name = 'shivam'
+var school = 'sliver bell'
+var rollno = '49'
+var grade = 'A'
+var english = 90
+var hindi = 80
+var maths = 90
+
+console.log(name)
+console.log(school)
+console.log(rollno)
+console.log(grade)
+console.log(english)
+console.log(hindi)
+console.log(maths)
