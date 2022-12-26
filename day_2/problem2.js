6d1f4ec905121fdb62a9b0a0e06a4fe99a7a4e84
shivamvr <shivamweb2@gmail.com>
2022-12-26 16:38:38 +0530

    day-2

---
+++ day_2/problem2.js
@@ -0,0 +1,6 @@
+var name = 'Shivam'
+console.log(name)
+name = 'Rajkumar'
+console.log(name)
+name = 'Rekha'
+console.log(name)
---
+++ day_2/problem3.js
@@ -0,0 +1,4 @@
+var name = 'shivam'
+var age = '24'
+console.log(name)
+console.log(age)
